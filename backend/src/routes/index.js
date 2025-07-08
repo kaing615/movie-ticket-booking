68057499d7997c8304c30ec62368368260747293
kaing615 <kainguyen615@gmail.com>
2025-07-08 19:10:55 +0700

    feat(routes): Add userRoute to index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,8 @@
 import express from "express";
+import userRoute from "./user.route.js";
 
 const router = express.Router();
 
-// Import all route files here
+router.use('/user', userRoute);
 
 export default router;
