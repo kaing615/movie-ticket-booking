7f39be8e3716a46006c8a4bf1726fc25a6f73efc
Ianlent <invisiblefullmoon2@gmail.com>
2025-07-08 21:23:09 +0700

    Added middleware for RBAC

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,14 @@
 import express from "express";
+import token from "../middlewares/token.middleware.js";
+import authorizeRoles from "../middlewares/role.middleware.js"; // Middleware to authorize user roles
+
 import userRoute from "./user.route.js";
 
 const router = express.Router();
 
-router.use('/user', userRoute);
+router.use("/user", userRoute);
+
+//Every route after this middleware will require authentication
+router.use(token);
 
 export default router;
