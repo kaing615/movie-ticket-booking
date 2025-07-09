fa6e0d7f6665b19f42d5723b3e6676d417abb62e
Ianlent <invisiblefullmoon2@gmail.com>
2025-07-09 21:55:53 +0700

    - Chỉnh lại API response khi signup và signin - Thiết kế trang đăng nhập - Nối API signin trang đăng nhập - Tạo auth slice để xử lý phân quyền và quản lý state người dùng - Phân route cho admin và manager(chưa test cho manager)

--- frontend/src/redux/features/user.slice.js
+++ frontend/src/redux/features/user.slice.js
@@ -3,16 +3,16 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = null;
 
 const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    setUser: (state, action) => {
-      return action.payload;
-    },
-    clearUser: (state) => {
-      return null;
-    },
-  },
+	name: "loading",
+	initialState,
+	reducers: {
+		setUser: (state, action) => {
+			return action.payload;
+		},
+		clearUser: (state) => {
+			return null;
+		},
+	},
 });
 
 export const { setUser, clearUser } = userSlice.actions;
