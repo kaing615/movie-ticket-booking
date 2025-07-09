193b2af3fa70ea9620be73a05c928f0c7e80132d
TruongVan-25 <van.truong.cit23@eiu.edu.vn>
2025-07-09 20:59:09 +0700

    after merge with main

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,7 @@ import authorizeRoles from "../middlewares/role.middleware.js"; // Middleware to
 
 import userRoute from "./user.route.js";
 import theaterRoute from "./theater.route.js";
+import theaterManagerRoute from "./theater-manager.route.js"
 
 const router = express.Router();
 
@@ -11,7 +12,7 @@ router.use("/user", userRoute);
 
 //Every route after this middleware will require authentication
 router.use(token.auth);
-
+router.use("/theater-manager", authorizeRoles("theater-manager"), theaterManagerRoute);
 router.use("/theater", authorizeRoles(["admin"]), theaterRoute);
 
 export default router;
